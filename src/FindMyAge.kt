d27f1674b1ad78f8798fb6b75fc99170ee591984
Rafael Stoner <rafaelstoner7@gmail.com>
2020-04-26 00:40:57 +0200

    Find My age app update

--- src/FindMyAge.kt
+++ src/FindMyAge.kt
@@ -3,11 +3,11 @@ import java.util.*
 fun main(){
 
     //Pedido de data de nascimento
-    print("Introduza  o ano de nascimento")
+    print("Introduza  o ano de nascimento: ")
     val yearOfBirth = readLine()!!.toInt()
     val yearInDevice = Calendar.getInstance().get(Calendar.YEAR)
     val age = yearInDevice - yearOfBirth
 
-    print("voce tem $age anos de idade ")
+    println("voce tem $age anos de idade ")
 
 }
