fe96ea62511345388f2db555a0d76c86c5d65d7a
Rafael Stoner <rafaelstoner7@gmail.com>
2020-04-26 02:50:40 +0200

    simple if

--- src/SimpleIF.kt
+++ src/SimpleIF.kt
@@ -7,16 +7,23 @@ fun main(){
     print("Introduza a idade ")
     val age= readLine()!!.toInt()
 
+    if(age <13)
+        print("Crianca")
+
     if(age in 13..17)
         print("Adolescente")
+
     if(age in 18..21)
         print("Jovem")
+
     if(age in 21..45)
         print("Adulto")
 
+    if(age >45)
+        print("Velho")
 
 
-
+    println("")
     println("Fim do App")
 
 
