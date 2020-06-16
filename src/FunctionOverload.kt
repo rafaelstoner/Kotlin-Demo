77dd51fbf6261c02f85063b4c6672d04687f0279
Rafael Stoner <rafaelstoner7@gmail.com>
2020-06-16 11:52:16 +0200

    Function Overload

--- src/FunctionOverload.kt
+++ src/FunctionOverload.kt
@@ -2,16 +2,16 @@
 
 
 fun  add(number1:Int, number2:Int):Int{
-    return number1+number2
+    return number1 + number2
 }
 
 
 fun  add(number1:Int, number2:Int,number3:Int):Int{
-    return number1+number2+ number3
+    return number1 + number2 + number3
 }
 
 fun  add(number1:Int, number2:Int, number3:Int, number4:Int):Int{
-    return number1+number2+number3 + number4
+    return number1 + number2 + number3 + number4
 }
 
 
