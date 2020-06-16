196fbbc9eaa5fdbf0a1dac3f16968d71394a3e9c
Rafael Stoner <rafaelstoner7@gmail.com>
2020-06-16 11:14:39 +0200

    Collections

--- src/CollectionUpdate.kt
+++ src/CollectionUpdate.kt
@@ -9,23 +9,22 @@ fun main(){
     //1- list
 
     //im-mutable
-    val listImmutable = listOf("Jena","laya")
-    //list[0]="Hussein"
+    val listImmutable = listOf("Rafa","Stoner")
     for (name in listImmutable){
         println(name)
     }
 
     //mutable
-    var listMutable = mutableListOf("Jena","laya")
-    listMutable[0]="Hussein"
+    var listMutable = mutableListOf("Rafa","Stoner")
+    listMutable[0]="Daniel"
     for (name in listMutable){
         println(name)
     }
 
 
-    val listOfUsersI = mapOf(1 to "hussein", 2 to "jena")
+    val listOfUsersI = mapOf(1 to "Rafa", 2 to "Stoner")
 
-    var listOfUsersM = mutableMapOf(1 to "hussein", 2 to "jena")
+    var listOfUsersM = mutableMapOf(1 to "Rafa", 2 to "Stoner")
 
 
 
