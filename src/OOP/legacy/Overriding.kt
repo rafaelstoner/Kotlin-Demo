d3ab2afd3231ac4dd6ab92a92374b7a6c34d625f
Rafael Stoner <rafaelstoner7@gmail.com>
2020-06-19 14:44:50 +0200

    Override functions in Kotlin

--- src/OOP/legacy/Overriding.kt
+++ src/OOP/legacy/Overriding.kt
@@ -1,9 +1,10 @@
-package legacy
+package oop.legacy
 //Overriding process
 
+//the inherited class must have the "open" value
 open class Operations1 {
 
-    //the overrided class should be open
+    //the overrode method must have the "open" value
     open fun sum(n1:Int,n2:Int):Int{
         return n1+n2
     }
@@ -12,15 +13,11 @@ open class Operations1 {
     }
 }
 
-class  MultiOperations1:Operations1{
-   constructor(): super()
+class  MultiOperations1 : Operations1() {
 
+    //override function
     override fun sum(n1:Int,n2:Int):Int{
-
-
-
         return n1+n2*3
-
     }
     fun sub(n1:Int,n2:Int):Int{
         return n1-n2
@@ -31,21 +28,25 @@ class  MultiOperations1:Operations1{
 }
 
 
-fun main(args:Array<String>){
+fun main(){
 
-    var op=Operations1()
+    //first
+    val op= Operations1()
     var sum=op.sum(10,15)
-    println("sum:"+ sum)
+    println("sum:$sum")
     var div= op.div(12,11)
-    println("div:"+ div)
+    println("div:$div")
 
     // second
-    var op2=MultiOperations1()
+    val op2= MultiOperations1()
     sum=op2.sum(10,15)
-    println("sum:"+ sum)
+    println("sum:$sum")
     div= op2.div(12,11)
-    println("div:"+ div)
-
+    println("div:$div")
+    val sub=op2.sub(10,5)
+    println("sub: $sub")
+    val mul=op2.mul(5,5)
+    println("mul: $mul")
 
 
 }
